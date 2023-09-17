7d62320626bbfaca92e3b03378458ae485088e1d
Josemy Duarte <duartejosemy@gmail.com>
2023-09-17 19:03:07 +0200

    Center buttons

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -144,18 +135,24 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
         persistentFooterButtons: <Widget>[
-          TextButton(
-            onPressed: _durations.length > 0 ? _clearPresses : null,
-            child: const Icon(Icons.bolt),
-          ),
-          TextButton(
-            onPressed:
-                !_isRecording && _durations.length > 0 ? _playRecording : null,
-            child: const Icon(Icons.play_arrow),
-          ),
-          TextButton(
-            onPressed: _isRecording ? _stopRecording : null,
-            child: const Icon(Icons.stop),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+            children: <Widget>[
+              TextButton(
+                onPressed: _durations.length > 0 ? _clearPresses : null,
+                child: const Icon(Icons.bolt),
+              ),
+              TextButton(
+                onPressed: !_isRecording && _durations.length > 0
+                    ? _playRecording
+                    : null,
+                child: const Icon(Icons.play_arrow),
+              ),
+              TextButton(
+                onPressed: _isRecording ? _stopRecording : null,
+                child: const Icon(Icons.stop),
+              ),
+            ],
           ),
         ],
       ),
